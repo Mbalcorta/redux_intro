fcd9c50c760159b4e94e4b42cfa6582423bed9e0
Mateo Balcorta <misterrattail@gmail.com>
2018-10-11 11:05:48 -0700

    Add webpack.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div className="container">
+    <p>This is redux</p>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
